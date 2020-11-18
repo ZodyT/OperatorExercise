69bab2d0eda52baf1ca59d458f58432f420a4de5
ZodyT <Zoraida1988@yahoo.com>
2020-11-18 13:35:56 -0600

    Finished

--- Program.cs
+++ Program.cs
@@ -16,6 +16,14 @@ namespace OperatorExercise
             Console.WriteLine(a - b);
             Console.WriteLine(a * b);
             Console.WriteLine(a / b);
+
+            Console.WriteLine("What is the radius of your circle?");
+            var r = double.Parse(Console.ReadLine());
+            
+            double pi = 3.14159;
+            double AreaOfCircle = (pi * (r * r));       
+
+            Console.WriteLine($"The area of a circle with radius of {r} is {AreaOfCircle}");
         }
     }
 }
